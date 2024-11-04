039fb15f6713e3e91f3f40a3fce7eca3bbb338c8
piusatwau <atwaupius7@gmail.com>
2024-11-04 11:25:20 +0530

    implemented robust user authentification system in django

--- practice/django_auth/django_project/django_project/settings.py
+++ practice/django_auth/django_project/django_project/settings.py
@@ -132,3 +132,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = "home" 
 LOGOUT_REDIRECT_URL = "home"
+
+# Outputting emails to console instead of default SMTP
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend" 
