8d4d131523b94ac3fdee75e4db3c6efac153c0bd
piusatwau <atwaupius7@gmail.com>
2024-11-09 21:00:42 +0530

    learnt the actual meaning and purpose of django ORM

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -10,5 +10,5 @@ class BookAdmin(admin.ModelAdmin):
     list_filter = ('publication_year')
     search_fields = ('title', 'author')
    
-    
+# this actually registers a  model in the admin panel    
 admin.site.register(Book, BookAdmin)
