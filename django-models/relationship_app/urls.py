e7ee083b19bed94a7b403760b74ca2e240628200
piusatwau <atwaupius7@gmail.com>
2024-11-10 02:33:35 +0530

    error fix

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -9,7 +9,7 @@ from .views import SignOutView
 urlpatterns = [
     path('list/', list_books, name="book_list"),
     path('details/', LibraryDetailView.as_view(), name='book_details'),
-    path('register/', SignUpView.as_view(), name="register"),
-    path('login/', SignInView.as_view(), name='login'),
-    path('logout/', SignOutView.as_view(), name="logout"),
+    path('register/', SignUpView.as_view(template_name='register.html'), name="register"),
+    path('login/', SignInView.as_view(template_name='logout.html'), name='login'),
+    path('logout/', SignOutView.as_view(template_name='login.html'), name="logout"),
 ]
