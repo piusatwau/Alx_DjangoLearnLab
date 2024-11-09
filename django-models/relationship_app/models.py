2de4a17a33c0dbe1a9912279b05fc934a199d2c2
piusatwau <atwaupius7@gmail.com>
2024-11-09 21:55:29 +0530

    eror fix

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -7,11 +7,17 @@ from django.db import models
 class Author(models.Model):
     name=models.CharField(max_length=200)
     
+    def __str__(self):
+        return self.name
+    
 
 # Book Model
 class Book(models.Model):
     title=models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    
+    def __str__(self):
+        return self.name
     
 # Library Model
 
@@ -19,9 +25,15 @@ class Library(models.Model):
     name = models.CharField(max_length=200)
     books = models.ManyToManyField(Book, related_name ='libraries')
     
+    def __str__(self):
+        return self.name
+    
 
 # Librarian Models
 
 class Librarian(models.Model):
     name = models.CharField(max_length=200)
     library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')
+    
+    def __str__(self):
+        return self.name
