04323789006f918b1b4eefc8c903a942cd5e8e3e
piusatwau <atwaupius7@gmail.com>
2024-11-30 01:32:31 +0530

    first task

--- advanced-api-project/advanced_api_project/api/models.py
+++ advanced-api-project/advanced_api_project/api/models.py
@@ -3,8 +3,16 @@ from django.db import models
 # Create your models here.
 
 class Author(models.Model):
-    pass 
+    name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
 
 
 class Book(models.Model):
-    pass
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    title = models.CharField(max_length=255)
+    publication_year = models.IntegerField()
+    
+    def __str__(self):
+        return self.title
