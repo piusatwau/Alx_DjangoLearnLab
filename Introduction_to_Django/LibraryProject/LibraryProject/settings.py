49456caa5d42f3fceb3f618a4f36fef1f324a478
Atwau Pius <atwaupius7@gmail.com>
2025-01-24 11:35:43 +0530

    changed database engine to postgres

--- Introduction_to_Django/LibraryProject/LibraryProject/settings.py
+++ Introduction_to_Django/LibraryProject/LibraryProject/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'LibraryProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'LibraryProject',
+        'HOST': 'localhost',
+        'PORT': '5433   ',
+        'USER': 'postgres',
+        'PASSWORD': '72527',
+        
     }
 }
 
